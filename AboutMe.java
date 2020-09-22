2fa672d67d52529f70a95b0f1d9f98f4d46238d2
Ryan Dubeau <Ryan.dubeau692@sccdsb.net>
2020-09-22 14:03:34 -0400

    Update AboutMe.java

--- AboutMe.java
+++ AboutMe.java
@@ -1,22 +1,27 @@
 /*
- * Programmer:
- * Date:
- * Purpose:
+ * Programmer: Ryan Dubeau
+ * Date: 9/22/2020
+ * Purpose: To print my name, age in height from storing it in a variable
  */
 
 public class AboutMe {
     public static void main (String [] args) {
         // Declaring the variables
         // TODO: Write the code the declares the variables
-
-        // Print the values
+    	
+    	String myName = "Ryan"
+    	int myAge = 16
+    	double myHeightInMeters = 1.56
+        
+    	// Print the values
         System.out.println("Hi, my name is " + myName);
         System.out.println("I am " + myAge + " years old.");
         System.out.println("My height is " + myHeightInMetres + " m.");
 
         // Change the variables
         // TODO: Write the code that changes the variables
-
+        myAge = 17
+        myHeightInMeters = 1.58
         // Print the new values
         System.out.println("Some time passes...");
         // TODO: Write the last two print statements
